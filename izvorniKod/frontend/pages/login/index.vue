ffb0f45366d77977651ebcbc33762007abbdcb0a
MarijaDragosevic <marija.dragosevic@fer.hr>
2020-11-01 17:34:43 +0100

    Login and register form style

--- izvorniKod/frontend/pages/login/index.vue
+++ izvorniKod/frontend/pages/login/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-    Login page
-    <input type="text" v-model="form.username">
-    <input type="text" v-model="form.password">
-    <button @click="loginUser">Prijavi se</button>
-  </div>
+  <div class="hero">
+      <div class="form-box">
+         <div class="button-box">
+            <div id="btn"></div>
+            <button type="button" class=" toggle-btn" @click="login=true">Log In</button>
+            <button type="button" class=" toggle-btn" @click="login=false">Register</button>
+         </div>
+         <form id="login" class="input-group">
+            <input type="text" class="input-field" placeholder="User id" required v-model="form.username">
+            <input type="text" class="input-field" placeholder="Enter Password" required v-model="form.password">
+            <input type="checkbox" class="check-box"><span>Remember password</span>
+            <button type="submit" class="submit-btn" @click="loginUser">Log in</button>
+         </form>
+         <form id="register" class="input-group">
+            <input type="text" class="input-field" placeholder="User id" required>
+            <input type="email" class="input-field" placeholder="Email id" required>
+            <input type="text" class="input-field" placeholder="Enter Password" required>
+            <input type="text" class="input-field" placeholder="Confirm Password" required>
+
+            <input type="checkbox" class="check-box"><span>I agree to the terms & conditions</span>
+            <button type="submit" class="submit-btn">Log in</button>
+         </form>
+      </div>
+   </div>
 </template>
 
 <script>
+      var x = document.getElementById("login");
+      var y = document.getElementById("register");
+      var z = document.getElementById("btn");
+
+      function register() {
+         x.style.left = "-400px"
+         y.style.left = "50px"
+         z.style.left = "110px"
+      }
+
+      function login() {
+         x.style.left = "50px"
+         y.style.left = "450px"
+         z.style.left = "0"
+      }
 
 export default {
 
@@ -16,7 +49,8 @@ export default {
       form: {
         username: 'janez',
         password: '12345678',
-      }
+      },
+      login: true
     };
   },
   methods: {
@@ -36,3 +70,111 @@ export default {
   }
 }
 </script>
+
+<style>
+* {
+   margin: 0;
+   padding: 0;
+   font-family: sans-serif;
+}
+
+.hero {
+   height: 100%;
+   width: 100%;
+   background-position: center;
+   background-size: cover;
+   background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(terminko1.jpg);
+   position: absolute;
+}
+
+.form-box {
+   height: 480px;
+   width: 380px;
+   position: relative;
+   margin: 6% auto;
+   background: #fff;
+   padding: 5px;
+   overflow: hidden;
+}
+
+.button-box {
+   width: 220px;
+   margin: 35px auto;
+   position: relative;
+   box-shadow: 0 0 20px 9px #1e9aa0b4;
+   border-radius: 30px;
+
+}
+
+.toggle-btn {
+   padding: 10px 30px;
+   cursor: pointer;
+   background: transparent;
+   border: 0;
+   outline: none;
+   position: relative;
+}
+
+#btn {
+   top: 0;
+   left: 0;
+   position: absolute;
+   width: 110px;
+   height: 100%;
+   background: linear-gradient(to right, #4e43e2, #4fdee6);
+   border-radius: 30px;
+   transition: .5s;
+}
+
+.input-group {
+   top: 120px;
+   position: absolute;
+   width: 280px;
+   transition: .5s;
+}
+
+.input-field {
+   width: 100%;
+   padding: 10px 0;
+   margin: 5px 0;
+   border-left: 0;
+   border-top: 0;
+   border-right: 0;
+   border-bottom: 1px solid #999;
+   outline: none;
+   background: transparent;
+}
+
+.submit-btn {
+   width: 85%;
+   padding: 10px 30px;
+   cursor: pointer;
+   display: block;
+   margin: auto;
+   background: linear-gradient(to right, #4e43e2, #4fdee6);
+   border: 0;
+   outline: none;
+   border-radius: 30px;
+
+
+}
+
+.check-box {
+   margin: 30px 10px 30px 0;
+}
+
+span {
+   color: #777;
+   font-size: 12px;
+   bottom: 68px;
+   position: absolute;
+}
+
+#login {
+   left: 50px;
+}
+
+#register {
+   left: 450px;
+}
+</style>
