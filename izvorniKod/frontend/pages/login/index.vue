9c62a43baaff3e61fd7b8af39e1188d1bbb35aa6
bronemos <spieglb@gmail.com>
2020-11-05 22:39:11 +0100

    Added profile/index

--- izvorniKod/frontend/pages/login/index.vue
+++ izvorniKod/frontend/pages/login/index.vue
@@ -1,186 +1,208 @@
 <template>
   <div class="hero">
-      <div class="form-box">
-         <div class="button-box">
-            <div id="btn" v-bind:class="login ? 'btn_0' : 'btn_110'"></div>
-            <button type="button" class=" toggle-btn" @click="login=true">Prijava</button>
-            <button type="button" class=" toggle-btn" @click="login=false">Registracija</button>
-         </div>
-         <form id="login" class="input-group" :class="login ? 'login_50' : 'login_n400'">
-            <input type="text" class="input-field" placeholder="Korisničko ime" required v-model="form.username">
-            <input type="password" class="input-field" placeholder="Lozinka" required v-model="form.password">
-            <input type="checkbox" class="check-box"><span>&nbsp;&nbsp;&nbsp;&nbsp;Zapamti zaporku</span>
-            <button type="button" class="submit-btn" @click="loginUser">Log in</button>  
-         </form>
+    <div class="form-box">
+      <div class="button-box">
+        <div id="btn" v-bind:class="login ? 'btn_0' : 'btn_110'"></div>
+        <button type="button" class=" toggle-btn" @click="login=true">Prijava</button>
+        <button type="button" class=" toggle-btn" @click="login=false">Registracija</button>
 
-         <form id="register" class="input-group" :class="login ? 'register_450' : 'register_50'">
-            <input type="text" class="input-field" placeholder="Korisničko ime" required v-model="r_form.username">
-            <input type="email" class="input-field" placeholder="Email" required v-model="r_form.email">
-            <input type="password" class="input-field" placeholder="Lozinka" required v-model="r_form.password">
-            <input type="password" class="input-field" placeholder="Ponovite lozinku" required v-model="r_form.password_again">
-            <input type="checkbox" class="check-box" > <span> &nbsp;&nbsp;&nbsp;&nbsp; Slažem se s uvjetima & odredbama</span>
-            <button type="button" class="submit-btn">Register</button>
-         </form>
       </div>
-   </div>
+      <form id="login" class="input-group" :class="login ? 'login_50' : 'login_n400'">
+        <input type="text" class="input-field" placeholder="Korisničko ime" required v-model="form.username">
+        <input type="password" class="input-field" placeholder="Lozinka" required v-model="form.password">
+        <input type="checkbox" class="check-box"><span>&nbsp;&nbsp;&nbsp;&nbsp;Zapamti zaporku</span>
+        <button type="button" class="submit-btn" @click="loginUser">Log in</button>
+      </form>
+
+      <form id="register" class="input-group" :class="login ? 'register_450' : 'register_50'">
+        <input type="text" class="input-field" placeholder="Korisničko ime" required v-model="r_form.username">
+        <input type="email" class="input-field" placeholder="Email" required v-model="r_form.email">
+        <input type="password" class="input-field" placeholder="Lozinka" required v-model="r_form.password">
+        <input type="password" class="input-field" placeholder="Ponovite lozinku" required
+               v-model="r_form.password_again">
+        <input type="checkbox" class="check-box">
+        <span> &nbsp;&nbsp;&nbsp;&nbsp; Slažem se s uvjetima & odredbama</span>
+        <button type="button" class="submit-btn">Register</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+  export default {
 
-  data() {
-    return {
-      form: {
-        username: '',
-        password: '',
-      },
-      login: true,
-      r_form: {
-        username: '',
-        email: '',
-        password: '',
-        password_again: '',
-      }
+    data() {
+      return {
+        form: {
+          username: '',
+          password: '',
+        },
+        login: true,
+        r_form: {
+          username: '',
+          email: '',
+          password: '',
+          password_again: '',
+        }
 
-    };
-  },
-  methods: {
-   async loginUser() {
-      try {
-        let response = await this.$axios.post(`/account/login/`, this.form);
-        this.$toast.show("Zahtjev uspješno poslan!", { duration: 8000 });
-        this.$store.commit('User/SET_LOGGED_USER', response.data.user);
-
-        // redirect to user profile
-        //this.$router.push('/')
-
-      } catch (e) {
-        this.$toast.error(`${e.response.status} ${e.response.statusText}`, { duration: 8000 });
-      }
+      };
     },
+    methods: {
+      async loginUser() {
+        try {
+          //let response = await this.$axios.post(`/account/login/`, this.form);
+          //this.$toast.show("Zahtjev uspješno poslan!", { duration: 8000 });
+          //this.$store.commit('User/SET_LOGGED_USER', response.data.user);
+
+          // redirect to user profile
+          await this.$router.push('/profile')
+
+        } catch (e) {
+          this.$toast.error(`${e.response.status} ${e.response.statusText}`, {duration: 8000});
+        }
+      },
+    }
   }
-}
 </script>
 
 <style>
-* {
-   margin: 0;
-   padding: 0;
-   font-family: sans-serif;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
 
-.login_50 {
-  left: 50px;
-}
+  .login_50 {
+    left: 50px;
+  }
 
-.login_n400 {
-  left: -400px;
-}
+  .login_n400 {
+    left: -400px;
+  }
 
-.register_450 {
-  left: 450px;
-}
+  .register_450 {
+    left: 450px;
+  }
 
-.register_50 {
-  left: 50px;
-}
+  .register_50 {
+    left: 50px;
+  }
 
-.btn_0 {
-  left: 0px
-}
+  button:focus{
+    outline: none !important;
+  }
 
-.btn_110 {
-  right: 110px;
-}
+  .btn_0 {
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 120px;
+    height: 100%;
+    background: linear-gradient(to right, #4e43e2, #4fdee6);
+    border-radius: 30px;
+    transition: .5s;
+  }
 
-.hero {
-   height: 100%;
-   width: 100%;
-   background-position: center;
-   background-size: cover;
-   background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('~@/static/images/terminko1.jpg');
-   position: absolute;
-}
+  .btn_110 {
+    left: 110px;
+    top: 0;
+    position: absolute;
+    width: 150px;
+    height: 100%;
+    background: linear-gradient(to right, #4e43e2, #4fdee6);
+    border-radius: 30px;
+    transition: .5s;
+  }
 
-.form-box {
-   height: 480px;
-   width: 380px;
-   position: relative;
-   margin: 6% auto;
-   background: #fff;
-   padding: 5px;
-   overflow: hidden;
-}
+  .hero {
+    height: 100%;
+    width: 100%;
+    background-position: center;
+    background-size: cover;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('~@/static/images/terminko1.jpg');
+    position: absolute;
+  }
 
-.button-box {
-   width: 260px;
-   margin: 35px auto;
-   position: relative;
-   box-shadow: 0 0 20px 9px #1e9aa0b4;
-   border-radius: 30px;
+  .form-box {
+    height: 480px;
+    width: 380px;
+    position: relative;
+    margin: 6% auto;
+    background: #fff;
+    padding: 5px;
+    overflow: hidden;
+  }
 
-}
+  .button-box {
+    width: 260px;
+    margin: 35px auto;
+    position: relative;
+    box-shadow: 0 0 20px 9px #1e9aa0b4;
+    border-radius: 30px;
 
-.toggle-btn {
-   padding: 10px 30px;
-   cursor: pointer;
-   background: transparent;
-   border: 0;
-   outline: none;
-   position: relative;
-}
+  }
 
-#btn {
-   top: 0;
-   left: 0;
-   position: absolute;
-   width: 120px;
-   height: 100%;
-   background: linear-gradient(to right, #4e43e2, #4fdee6);
-   border-radius: 30px;
-   transition: .5s;
-}
+  .toggle-btn {
+    padding: 10px 30px;
+    cursor: pointer;
+    background: transparent;
+    border: 0;
+    outline: none;
+    position: relative;
+  }
 
-.input-group {
-   top: 120px;
-   position: absolute;
-   width: 280px;
-   transition: .5s;
-}
+  .btn {
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 120px;
+    height: 100%;
+    background: linear-gradient(to right, #4e43e2, #4fdee6);
+    border-radius: 30px;
+    transition: .5s;
+  }
 
-.input-field {
-   width: 100%;
-   padding: 10px 0;
-   margin: 5px 0;
-   border-left: 0;
-   border-top: 0;
-   border-right: 0;
-   border-bottom: 1px solid #999;
-   outline: none;
-   background: transparent;
-}
+  .input-group {
+    top: 120px;
+    position: absolute;
+    width: 280px;
+    transition: .5s;
+  }
 
-.submit-btn {
-   width: 85%;
-   padding: 10px 30px;
-   cursor: pointer;
-   display: block;
-   margin: auto;
-   background: linear-gradient(to right, #4e43e2, #4fdee6);
-   border: 0;
-   outline: none;
-   border-radius: 30px;
+  .input-field {
+    width: 100%;
+    padding: 10px 0;
+    margin: 5px 0;
+    border-left: 0;
+    border-top: 0;
+    border-right: 0;
+    border-bottom: 1px solid #999;
+    outline: none;
+    background: transparent;
+  }
+
+  .submit-btn {
+    width: 85%;
+    padding: 10px 30px;
+    cursor: pointer;
+    display: block;
+    margin: auto;
+    background: linear-gradient(to right, #4e43e2, #4fdee6);
+    border: 0;
+    outline: none;
+    border-radius: 30px;
 
 
-}
+  }
 
-span {
-   color: #777;
-   font-size: 12px;
-   bottom: 68px;
-   position: absolute;
-}
-.check-box {
-   margin: 20px 200px 30px 0px;
-}
+  span {
+    color: #777;
+    font-size: 12px;
+    bottom: 68px;
+    position: absolute;
+  }
+
+  .check-box {
+    margin: 20px 200px 30px 0px;
+  }
 </style>
--- izvorniKod/frontend/pages/profile/index.vue
+++ izvorniKod/frontend/pages/profile/index.vue
@@ -1,13 +1,136 @@
 <template>
-    $END$
+  <div>
+    <div class="form-box">
+      <form class="input-group"></form>
+    </div>
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "profile.vue"
-    }
+  export default {}
 </script>
 
-<style scoped>
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
 
+  .login_50 {
+    left: 50px;
+  }
+
+  .login_n400 {
+    left: -400px;
+  }
+
+  .register_450 {
+    left: 450px;
+  }
+
+  .register_50 {
+    left: 50px;
+  }
+
+  .btn_0 {
+    left: 0px
+  }
+
+  .btn_110 {
+    right: 110px;
+  }
+
+  .hero {
+    height: 100%;
+    width: 100%;
+    background-position: center;
+    background-size: cover;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('~@/static/images/terminko1.jpg');
+    position: absolute;
+  }
+
+  .form-box {
+    height: 480px;
+    width: 380px;
+    position: relative;
+    margin: 6% auto;
+    background: #fff;
+    padding: 5px;
+    overflow: hidden;
+  }
+
+  .button-box {
+    width: 260px;
+    margin: 35px auto;
+    position: relative;
+    box-shadow: 0 0 20px 9px #1e9aa0b4;
+    border-radius: 30px;
+
+  }
+
+  .toggle-btn {
+    padding: 10px 30px;
+    cursor: pointer;
+    background: transparent;
+    border: 0;
+    outline: none;
+    position: relative;
+  }
+
+  #btn {
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 120px;
+    height: 100%;
+    background: linear-gradient(to right, #4e43e2, #4fdee6);
+    border-radius: 30px;
+    transition: .5s;
+  }
+
+  .input-group {
+    top: 120px;
+    position: absolute;
+    width: 280px;
+    transition: .5s;
+  }
+
+  .input-field {
+    width: 100%;
+    padding: 10px 0;
+    margin: 5px 0;
+    border-left: 0;
+    border-top: 0;
+    border-right: 0;
+    border-bottom: 1px solid #999;
+    outline: none;
+    background: transparent;
+  }
+
+  .submit-btn {
+    width: 85%;
+    padding: 10px 30px;
+    cursor: pointer;
+    display: block;
+    margin: auto;
+    background: linear-gradient(to right, #4e43e2, #4fdee6);
+    border: 0;
+    outline: none;
+    border-radius: 30px;
+
+
+  }
+
+  span {
+    color: #777;
+    font-size: 12px;
+    bottom: 68px;
+    position: absolute;
+  }
+
+  .check-box {
+    margin: 20px 200px 30px 0px;
+  }
 </style>
