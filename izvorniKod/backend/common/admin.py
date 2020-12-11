626557a980a7489122e8b83dce540dd0b2c0e017
Jan Grgić <jan.grgic@fer.hr>
2020-12-11 16:46:43 +0100

    models update

--- izvorniKod/backend/common/admin.py
+++ izvorniKod/backend/common/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from common.models import User
+from common.models import User, Review
 
 
 admin.site.register(User)
+admin.site.register(Review)
--- izvorniKod/backend/common/models.py
+++ izvorniKod/backend/common/models.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 
 from django.core.mail import send_mail
 from django.db import models
@@ -24,6 +25,17 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     )
 
 
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value': self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
+
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     JMBAG = models.CharField(null=False, unique=True, blank=False, max_length=10, default='xxx')
@@ -31,8 +43,8 @@ class User(AbstractUser):
     negative_points = models.IntegerField(null=False, blank=False, default=0)
 
 
-class WashDay(models.Model):
-    date = models.DateField(unique=True, editable=False)
+class Laundry(models.Model):
+    date_changed = models.DateTimeField(auto_now_add=True)
     open_time = models.TimeField(null=False, blank=False)
     close_time = models.TimeField(null=False, blank=False)
     pause_start = models.TimeField(null=False, blank=False)
@@ -40,20 +52,31 @@ class WashDay(models.Model):
     wash_price = models.FloatField(null=False, blank=False)
     drying_price = models.FloatField(null=False, blank=False)
 
+    class Meta:
+        ordering = ['-date_changed']
+
 
 class Machine(models.Model):
     type = models.CharField(max_length=10, choices=[('washer', 'washer'), ('dryer', 'dryer')])
 
 
 class Appointment(models.Model):
-    time = models.DateField(unique=True, editable=False)
+    start_time = models.DateField(null=False, editable=False, blank=True, default=datetime.now)
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
-    price = models.FloatField(null=False, blank=False)
+    price = models.FloatField(null=True, blank=True)
     note = models.TextField(null=True, blank=True)
     paid = models.BooleanField(null=False, blank=False)
-    basket_taken = models.BooleanField(null=False, blank=False)
+    basket_taken = models.BooleanField(null=True, default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointment')
-    employee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointment_worked')
+    employee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='appointment_worked', blank=True,
+                                 null=True)
+
+    def save(self, *args, **kwargs):
+        if self.machine.type == 'washer':
+            self.price = Laundry.objects.all().first().wash_price
+        else:
+            self.price = Laundry.objects.all().first().drying_price
+        super(Appointment, self).save(*args, **kwargs)
 
 
 class Post(models.Model):
@@ -71,4 +94,4 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recension_written')
     employee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recension')
     text = models.TextField(null=True, blank=True)
-    grade = models.IntegerField(null=False, blank=False)
+    grade = IntegerRangeField(null=False, blank=False, min_value=1, max_value=5)
