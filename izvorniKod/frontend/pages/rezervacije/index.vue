dd4989bd51a6f9faa54bf1d698bad16de56f5aac
dinogrgic1 <dino.grgic1@gmail.com>
2020-12-16 22:09:31 +0100

    Added on click functionality on calendar

--- izvorniKod/frontend/pages/rezervacije/index.vue
+++ izvorniKod/frontend/pages/rezervacije/index.vue
@@ -1,89 +1,109 @@
 <template class="background">
-  <!-- App.vue -->
-  <div id="app" data-app app-data="true" >
-  <div class="background">
-    <div class="hero">
-      <div class="container emp-profile">
-        <no-ssr>
-          <vue-cal
-            ref="vuecal"
-            :time-from="6 * 60"
-            :time-step="60"
-            locale="hr"
-            :disable-views="['years', 'year']"
-            class="vuecal--blue-theme"
-            today-button
-            :selected-date="selectedDate"
-            :min-date="minDate"
-            :max-date="maxDate"
-            :hide-weekdays="[7]"
-            editable-events
-            :events="events"
-            :split-days="splitDays"
-            :sticky-split-labels="stickySplitLabels"
-            :min-cell-width="minCellWidth"
-            :min-split-width="minSplitWidth"
-            :on-event-click="onEventClick"
-          >
-            <template v-slot:today-button>
-              <!-- Using Vuetify -->
-              <v-tooltip>
-                <template v-slot:activator="{ on }">
-                  <v-btn v-on="on">
-                    <span>Danas</span>
-                  </v-btn>
-                </template>
-              </v-tooltip>
-            </template>
-          </vue-cal>
-        </no-ssr>
+  <div id="app" data-app app-data="true">
+    <div class="background">
+      <div class="hero">
+        <div class="container emp-profile">
+          <client-only>
+            <vue-cal
+              ref="vuecal"
+              :time-from="6 * 60"
+              :time-step="60"
+              locale="hr"
+              :disable-views="['years', 'year']"
+              class="vuecal--blue-theme"
+              today-button
+              :selected-date="todayDate"
+              :min-date="minDate"
+              :max-date="maxDate"
+              :hide-weekdays="[7]"
+              :events="events"
+              :split-days="splitDays"
+              :sticky-split-labels="stickySplitLabels"
+              :min-cell-width="minCellWidth"
+              :min-split-width="minSplitWidth"
+              :on-event-click="onEventClick"
+            >
+              <template v-slot:today-button>
+                <!-- Using Vuetify -->
+                <v-tooltip>
+                  <template v-slot:activator="{ on }">
+                    <v-btn v-on="on">
+                      <span>Danas</span>
+                    </v-btn>
+                  </template>
+                </v-tooltip>
+              </template>
+            </vue-cal>
+          </client-only>
+        </div>
       </div>
+
+      <!-- click on event dialog -->
+      <v-dialog v-model="showDialog" width="unset">
+        <v-card>
+          <v-card-title>
+            <v-icon>{{ selectedEvent.icon }}</v-icon>
+            <span
+              >{{
+                selectedEvent.start && selectedEvent.start.format("DD.MM.YYYY")
+              }}    
+              {{ selectedEvent.label }}</span
+            >
+          </v-card-title>
+          <v-card-text v-if="selectedEvent.class == 'reserved'">
+            <span><strong>Plaćeno:</strong> {{ selectedEvent.paid }}</span
+            ><br />
+            <span
+              ><strong>Košara posuđena:</strong>
+              {{ selectedEvent.basket_taken }}</span
+            ><br />
+            <span><strong>Cijena:</strong> {{ selectedEvent.price }} kn</span
+            ><br />
+            <span><strong>Uređaj:</strong> {{ selectedEvent.machine }}</span
+            ><br />
+            <span><strong>Bilješka:</strong></span
+            ><br /><span>{{ selectedEvent.note }}</span>
+            <br />
+            <button v-if="user.is_staff" class="btn btn-danger">Obriši</button>
+          </v-card-text>
+
+          <v-card-text v-if="selectedEvent.class == 'free'">
+            <button class="btn btn-success">Rezerviraj</button>
+            <button class="btn btn-danger">Obriši</button>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+
+      <!-- loading dialog -->
+      <v-dialog v-model="loadingDialog" width="unset" v-if="loadingDialog">
+        <v-card>
+          <v-card-title class="d-flex justify-content-center">
+            <strong>Učitavanje rezervacija....</strong>
+          </v-card-title>
+          <v-card-text class="d-flex justify-content-center">
+            <img
+              src="https://media2.giphy.com/media/10etb2jVqCZYWc/giphy.gif"
+            />
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </div>
-    <!-- Using Vuetify -->
-    <v-dialog v-model="showDialog">
-      <v-card>
-        <v-card-title>
-          <v-icon>{{ selectedEvent.icon }}</v-icon>
-          <span>{{ selectedEvent.title }}</span>
-          <v-spacer />
-          <strong>{{
-            selectedEvent.start && selectedEvent.start.format("DD/MM/YYYY")
-          }}</strong>
-        </v-card-title>
-        <v-card-text>
-          <p v-html="selectedEvent.contentFull" />
-          <strong>Event details:</strong>
-          <ul>
-            <li>
-              Event starts at:
-              {{ selectedEvent.start && selectedEvent.start.formatTime() }}
-            </li>
-            <li>
-              Event ends at:
-              {{ selectedEvent.end && selectedEvent.end.formatTime() }}
-            </li>
-          </ul>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-  </div>
   </div>
 </template>
 
-
 <script>
-
 export default {
+  middleware: "auth",
+
   data: () => ({
-    selectedDate: new Date(),
+    loadingDialog: true,
+    todayDate: new Date(),
     stickySplitLabels: false,
-    minCellWidth: 400,
-    minSplitWidth: 200,
+    minCellWidth: 30,
+    minSplitWidth: 30,
     selectedEvent: {},
     showDialog: false,
     splitDays: [
-      // The id property is added automatically if none (starting from 1), but you can set a custom one.
-      // If you need to toggle the splits, you must set the id explicitly.
       { id: 1, class: "washer", label: "Perilica 1" },
       { id: 2, class: "dryer", label: "Sušilica 1" },
       { id: 3, class: "washer", label: "Perilica 2" },
@@ -96,34 +116,10 @@ export default {
       { id: 10, class: "dryer", label: "Sušilica 5" },
     ],
     events: [],
-    // events: [
-    //   {
-    //     start: "2020-12-15 14:00",
-    //     end: "2020-12-15 18:00",
-    //     title: "Need to go shopping",
-    //     content: "Click to see my shopping list",
-    //     contentFull:
-    //       "My shopping list is rather long:<br><ul><li>Avocados</li><li>Tomatoes</li><li>Potatoes</li><li>Mangoes</li></ul>", // Custom attribute.
-    //     class: "free",
-    //     split: 2
-    //   },
-    //   {
-    //     start: "2020-01-01 12:00",
-    //     end: "2020-01-01 13:00",
-    //     title: "Pauza",
-    //     class: "break",
-    //     background: true,
-    //     split: 2
-    //   }
-    // ]
   }),
 
   async fetch() {
-    this.events = [];
-    
     let res = await this.$axios.get(`laundry`);
-    let appointments = await this.$axios.get(`appointment`);
-    
     var data = res.data;
     let open_time = parseInt(
       data.open_time.substring(0, data.open_time.length - 6)
@@ -131,59 +127,83 @@ export default {
     let close_time = parseInt(
       data.close_time.substring(0, data.close_time.length - 6)
     );
+    let pause_start = data.pause_start.substring(0, data.close_time.length - 3);
+    let pause_end = data.pause_end.substring(0, data.close_time.length - 3);
+
+    var pause = {
+      start: pause_start + ":00",
+      end: pause_end + ":00",
+      class: "break",
+      title: "PAUZA",
+      label: "PAUZA",
+      background: true,
+    };
+
+    let appointments = await this.$axios.get(`appointment`);
     var that = this;
     appointments.data.forEach(function (app) {
-    // var x = arrayItem.prop1 + 2;
-      var event = {};
-      let date = app.start.substring(0,10);
-      let time = parseInt(app.start.substring(11,13))
-      
+      // var x = arrayItem.prop1 + 2;
+      var event = {
+        user_id: app.user_id,
+        note: app.note,
+        machine: app.title,
+        price: app.price,
+        paid: app.paid ? "DA" : "NE",
+        basket_taken: app.basket_taken ? "DA" : "NE",
+        employee: app.employee,
+      };
+      let date = app.start.substring(0, 10);
+      let time = parseInt(app.start.substring(11, 13));
+
       event.start = date + " " + time + ":00";
       event.end = date + " " + (time + 1) + ":00";
       event.class = "reserved";
-      event.title = `${time} - ${time + 1}`;
-      event.label = `${time} - ${time + 1}`;
-      if(app.machine.type=="washer"){
-        event.split = app.machine.id*2-1
-      }
-      else{
-        if(app.machine.id%2==0){
-          event.split = app.machine.id/2-1;
-        }
-        else{
-          event.split = app.machine.id/2+1;
+      event.title = `${time}:00 - ${time + 1}:00`;
+      event.label = `${time}:00 - ${time + 1}:00`;
+      if (app.machine.type == "washer") {
+        event.split = app.machine.id * 2 - 1;
+      } else {
+        if (app.machine.id % 2 == 0) {
+          event.split = app.machine.id / 2 - 1;
+        } else {
+          event.split = app.machine.id / 2 + 1;
         }
       }
-      event.user_id = app.user_id;
-      event.price = app.price;
-      event.paid = app.paid;
-      event.basket_taken = app.basket_taken;
-      event.employee = app.employee;
-      event.contentFull = `Košarica posuđena: ${event.basket_taken}, Plaćeno: ${event.paid}, Cijena: ${event.price}`
-      // console.log(event);
       that.events.push(event);
     });
 
-    var d = new Date();
-    for (var i = 1; i <= 14; i++) {
-      var date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();  
+    var last = this.todayDate.addDays(14);
+    var d = this.todayDate;
+    while (d < last) {
+      var date = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate();
       for (var j = open_time; j < close_time; j++) {
+        if (d == this.todayDate && j <= d.getHours()) continue;
+
         var event = {};
         event.start = date + " " + j + ":00";
         event.end = date + " " + (j + 1) + ":00";
         event.class = "free";
-        event.title = `${j} - ${j + 1}`;
+        event.title = `${j}:00 - ${j + 1}:00`;
         var c = 0;
-        event.label = `${j} - ${j + 1}`;
+        event.label = `${j}:00 - ${j + 1}:00`;
         for (var k = 1; k <= 10; k++) {
           let tmp = Object.assign({}, event);
+
+          if (j == parseInt(pause.start.substring(0, pause.start.length - 3))) {
+            let pauseEvent = Object.assign({}, pause);
+            pauseEvent.start = date + " " + pause.start;
+            pauseEvent.split = k;
+            pauseEvent.end = date + " " + pause.end;
+            this.events.push(pauseEvent);
+          }
+
           this.events.forEach(function (e) {
-            if(e.start== event.start && e.split==k){
-              c=1;
+            if (e.start == event.start && e.split == k) {
+              c = 1;
             }
           });
-          if(c==1){
-            c=0;
+          if (c == 1) {
+            c = 0;
             continue;
           }
           tmp.split = k;
@@ -192,21 +212,23 @@ export default {
       }
       d = d.addDays(1);
     }
-
-
+    this.loadingDialog = false;
   },
 
   methods: {
     onEventClick(event, e) {
       this.selectedEvent = event;
-      if (event.class != "break") {
-        this.showDialog = true;
-      }
-      // Prevent navigating to narrower view (default vue-cal behavior).
+      this.showDialog = true;
       e.stopPropagation();
     },
   },
   computed: {
+    user() {
+      if (this.$auth.loggedIn) {
+        return this.$auth.user;
+      }
+      return null;
+    },
     minDate() {
       return new Date();
     },
@@ -232,11 +254,12 @@ export default {
 .vuecal__event.free {
   background-color: rgba(179, 228, 213, 0.9);
   border: 1px solid rgba(162, 202, 190, 0.9);
-  color: #fff;
+  color: rgb(0, 0, 0, 0.5);
 }
 .vuecal__event.reserved {
   background-color: rgba(216, 226, 223, 0.9);
   border: 1px solid rgb(144, 210, 190);
+  color: rgb(0, 0, 0, 0.3);
 }
 
 .vuecal__cell--disabled {
@@ -279,10 +302,10 @@ export default {
     45deg,
     transparent,
     transparent 10px,
-    #f2f2f2 10px,
-    #f2f2f2 20px
+    #610606 10px,
+    #610606 20px
   ); /* IE 10+ */
-  color: #999;
+  color: rgb(0, 0, 0, 0.1);
   display: flex;
   justify-content: center;
   align-items: center;
